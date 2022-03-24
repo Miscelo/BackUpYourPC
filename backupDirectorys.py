4dc8deb8493617c2bfbb2a0b51e6f356f56f3a87
Mic <miscelo@tutanota.com>
2022-03-24 17:15:10 +0100

    Starting backup Project. Backup with shell on a personal Linux machine, checked all processes if Successfully with Python.

---
+++ backupDirectorys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+#list of all folders that will be backuped.
+backupfolders = ['/home/[user]/Documents', '/home/[user]/pro']
+
+#Make a control that all the given pathes exist on startup.
+def checkPathExists():
+    pass
+
+if __name__ == '__main__':
+    checkPathExists()
