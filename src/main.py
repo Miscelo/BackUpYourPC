cb61f2e0dd70269c576bf2c326c271113275b796
Mic <miscelo@tutanota.com>
2022-04-03 05:34:59 +0200

    Change Project Structur. Change name of file because of conflicts. Create more folders Add startmenu as function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
 
 import os
-from setuptools import getTime
-
-print("Show Path to backup:")
+from setuptools import getTime, createLog
 
 
 user = "mic"
 backupfile = "../data/backupfolders.txt"
 
+def startmenue(backupfile, user):
+    print("****** MENU *******")
+    print("(i) Insert new Backup folder")
+    print("(r) Remove Backup- Folders")
+    print("(o) Show Backup- folders")
+    print("(s) Send last backup- report.")
+    print("(m) Show logs.")
+    print("(c) General configuration")
+    print("(b) Start BACKUP")
+    print("(h) Help - First instructions")
+    letter = input("Your choice: ")
+    print(letter)
+    if(letter == "i"):
+        newFolder(backupfile, user)
+
+
+def newFolder(backupfile, user):
+    backuppath = input("Please write full path for your backup- folder or (Q)uit!")
+    if backuppath is not "Q":
+        with open(backupfile, "a") as f:
+            f.write(backuppath)
+    message = backuppath + "created and added to file " + backupfile +".:" + user
+    createLog(message)
+
+startmenue(backupfile, user)
+
+"""
 #Exist file with backup-path inside  /data/backupfolders.txt
 if(os.path.exists(backupfile)):
     with open(backupfile, "r") as f:
@@ -27,3 +52,8 @@ newfolder = input("Write path to backupfolder")
 
 date = getTime()
 print(date)
+"""
+
+
+# except PErmission error
+# except Filenot found error
